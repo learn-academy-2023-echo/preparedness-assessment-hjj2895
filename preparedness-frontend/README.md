0bcdfc4ca6ca2ad3908e49c89a782d9f30c8237b
peter ji <hyeongjun9@gmail.com>
2023-09-09 21:45:31 -0700

    frontend completed

--- preparedness-frontend/README.md
+++ preparedness-frontend/README.md
@@ -1,1 +1,22 @@
-# Preparedness Assessment
+# Preparedness AssessmentThe background is this image
+The modal has a header that greets the user with their name ✅
+The modal has a body with lorem ipsum filler text ✅
+The modal has a close button ✅
+The close button in modal closes the modal ✅
+
+The header text color is white and is 50px ✅
+The header is centered on the page ✅
+The input label text color is white and is 35px ✅
+The input label is centered on the page ✅
+The input field text is 18px ✅
+The input field should take up 50% of the width of the page ✅
+The input field is centered on the page ✅
+There is 12px of padding on the left and the right of the input field ✅
+There is 6px of padding on the top and the bottom of the input field ✅
+The buttons are centered under the input field ✅
+The background color of the buttons are #0dcaf0 ✅
+The color of the text on the buttons is white ✅
+Button width is set to 100px ✅
+There is 10px of padding on all sides of the button ✅
+There is 20px between the two buttons ✅
+There is 20px between the bottom of the input and the top of the buttons ✅
--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,20 +1,36 @@
-import React from "react"
-import { Button, Input, Label } from "reactstrap"
-import ModalComponent from "./components/ModalComponent"
-import "./App.css"
+import React, { useState } from "react";
+import { Button, Input, Label } from "reactstrap";
+import ModalComponent from "./components/ModalComponent";
+import "./App.css";
+import background from './assets/background-image.jpeg';
 
 const App = () => {
+
+  const [userInput, setUserInput] = useState("")
+
+  const handleChange = (e) => {
+    setUserInput(e.target.value)
+  }
+
+  const reset = () => {
+    setUserInput("")
+  }
+
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleOpenModal = () => setOpenModal(!openModal);
+  
   return (
-    <div className="entire-content">
+    <div className="entire-content" style={{backgroundImage: `url(${background})`}}>
       <h1>Preparedness Assessment</h1>
       <div className="form">
         <div className="input">
           <Label for="name">Enter your name</Label>
-          <Input />
+          <Input type="text" value={userInput} onChange={handleChange}/>
         </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
-        <ModalComponent />
+        <Button onClick={handleOpenModal}>Click Me</Button>
+        <Button onClick={reset}>Reset</Button>
+        {openModal && <ModalComponent toggleModal={handleOpenModal} inputValue={userInput}/>}
       </div>
     </div>
   )
